a94152139a2edbdfbcc0ec6aa596718fa69dcaae
范綱栓 <harry750110@gmail.com>
2025-05-20 17:16:45 +0800

    refactor: update project structure and configurations
    - Update Astro and Tailwind configurations
    - Reorganize project structure
    - Add new components and layouts
    - Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://harryfan.github.io',
-  base: "/harry-portfolio/", // 部署子目錄
+  site: 'https://astrofy-template.netlify.app',
+  // site: 'https://harryfan.github.io',
+  // base: "/harry-portfolio/", // 部署子目錄
+  integrations: [mdx(), sitemap(), tailwind()]
 });
