dd9739b4580d029b2dd3cff786eea317ac422673
范綱栓 <harry750110@gmail.com>
2025-05-20 17:37:07 +0800

    fix: update site and base URL configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://harryfan.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/harry-portfolio' : '', // 只在生產環境使用 base
+  site: 'https://harryfan.github.io/harry-portfolio',
+  base: '/harry-portfolio', // GitHub Pages 子目錄
   integrations: [mdx(), sitemap(), tailwind()],
   server: {
     port: 3000, // 指定固定端口
